93ce094bc88792d1fa4252953047cae402b1bea7
Olivia <yutongchen27@hotmail.com>
2025-02-18 10:50:23 -0800

    change massege in cal.js

--- cal.js
+++ cal.js
@@ -1,14 +1,19 @@
-// step 1 : creat three numbers with calculation.
-// equal 10
+// step 1 : Create a string(massege) for the user.
+// step 2 : creat three numbers with unique arithmetic operator.
+// step 3 :use alert to  combo string and numbers.( popup )
+// step 4 : creat a new GitHub repo.
+// step 5 : push files to repo.
+// xtop 6 : submit  GitHub repo link .
+
+//Create a string
+const massege = " You have received this message because you have been chosen to open an important vault : "
+// creat variable1 equal 10
 const num1 = 2 * 5;
-// equal 40 
+//creat variable2 equal 40 
 const num2 = 25 + 15;
-// equal 39
+// creat variable3 equal 39
 const num3 = 52 - 13;
-//step 2: combo thess numbers
- 
-const massege = " The combination of the lock is : "
-//use popup
+//use alert to  create a popup dialog.
 alert( `${massege} ${num1} - ${num2} - ${num3}`);
 
 
