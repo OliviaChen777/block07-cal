de1ca930227dedfd01548b0aee9a407be44b2d93
Olivia <yutongchen27@hotmail.com>
2025-02-18 10:18:18 -0800

    add two new files

---
+++ cal.js
@@ -0,0 +1,14 @@
+// step 1 : creat three numbers with calculation.
+// equal 10
+const num1 = 2 * 5;
+// equal 40 
+const num2 = 25 + 15;
+// equal 39
+const num3 = 52 - 13;
+//step 2: combo thess numbers
+ 
+const massege = " The combination of the lock is : "
+//use popup
+alert( `${massege} ${num1} - ${num2} - ${num3}`);
+
+
